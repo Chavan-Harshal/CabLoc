a4bcb2395ddd0f4ed558af5c738cdd42c174d3ba
NikitaTipule <nikitatipule@gmail.com>
2021-11-02 21:00:12 +0530

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css'
-import HomePage from './pages/HomePage'
-import CustomerLogin from './pages/CustomerLogin';
-import DriverLogin  from './pages/DriverLogin';
-import CustomerPage from './pages/CustomerPage';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import CustomerLogin from "./pages/CustomerLogin";
+// import DriverLogin from "./pages/DriverLogin";
+// s CustomerPage from './pages/CustomerPage';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    
     <div className="App">
       <BrowserRouter>
-        <Route exact path = '/' component = {HomePage} />
-        <Route exact path = '/CustomerLogin' component = {CustomerLogin} />
-        <Route exact path = '/DriverLogin' component = {DriverLogin} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/CustomerLogin" component={CustomerLogin} />
+        {/* <Route exact path="/DriverLogin" component={DriverLogin} /> */}
       </BrowserRouter>
     </div>
-    
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import './HomePage.css'
-import AppNavbar from './AppNavbar'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "./HomePage.css";
+import AppNavbar from "../components/AppNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class HomePage extends Component {
-
-    render() {
-        return (
-            <div>  
-                <AppNavbar />
-                <div className='bg-light'>   
-                    <div className='input'>
-                        <h1 className='header'>BOOK A TAXI TO YOUR<br /> DESTINATION</h1>
-                        <br /><span>Log in</span>
-                        <a href='/CustomerLogin' class='btn btn-outline-dark'>Customer</a>
-                        <a href='/DriverLogin' class='btn btn-outline-dark'>Driver</a>
-                        <a href='/AdminLogin' class='btn btn-outline-dark'>Admin</a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <AppNavbar />
+        <div className="bg-light">
+          <div className="input">
+            <h1 className="header">
+              BOOK A TAXI TO YOUR
+              <br /> DESTINATION
+            </h1>
+            <br />
+            <span>Log in</span>
+            <a href="/CustomerLogin" class="btn btn-outline-dark">
+              Customer
+            </a>
+            <a href="/DriverLogin" class="btn btn-outline-dark">
+              Driver
+            </a>
+            <a href="/AdminLogin" class="btn btn-outline-dark">
+              Admin
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default HomePage
+export default HomePage;
