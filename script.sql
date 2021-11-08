f01a0783dcd48802fe4e640b0ad939a523237c17
NikitaTipule <nikitatipule@gmail.com>
2021-11-08 10:37:49 +0530

    fixed bugs

--- script.sql
+++ script.sql
@@ -1,21 +1,19 @@
 CREATE TABLE user1 (
-    user_id int, 
+    user_id varchar(20), 
     name varchar(20),
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE user2 (
-    user_id int, 
+    user_id varchar(20), 
     phono_no int, 
-    PRIMARY KEY (user_id),
-    FOREIGN KEY (user_id) REFERENCES user1(user_id)
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE user3 (
-    user_id int, 
+    user_id varchar(20), 
     address varchar(20),
-    PRIMARY KEY (user_id),
-    FOREIGN KEY (user_id) REFERENCES user2(user_id)
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE user4 (
@@ -25,7 +23,7 @@ CREATE TABLE user4 (
 );
 
 CREATE TABLE shifts(
-    shift_id int,
+    shift_id varchar(20),
     start time, 
     end time,
     PRIMARY KEY (shift_id)
@@ -37,40 +35,8 @@ CREATE TABLE location(
     PRIMARY KEY (zipcode)
 );
 
-CREATE TABLE driver1 (
-    driver_id int,
-    d_name varchar(20),
-    d_phone_no int, 
-    taxi_id int, 
-    rating int,
-    PRIMARY KEY (driver_id, taxi_id),
-    FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id)
-);
-
-CREATE TABLE driver2 (
-    driver_id int, 
-    d_phone_no int,
-    PRIMARY KEY (driver_id),
-    FOREIGN KEY (driver_id) REFERENCES driver1(driver_id)
-);
-
-CREATE TABLE driver3 (
-    d_phone_no int, 
-    d_name varchar(20),
-    PRIMARY KEY (d_phone_no)
-);
-
-CREATE TABLE garage (
-    garage_id int, 
-    taxi_id int, 
-    status varchar(20),
-    PRIMARY KEY (garage_id, taxi_id),
-    FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id)
-);
- 
-
 CREATE TABLE taxi1 (
-    taxi_id int,
+    taxi_id varchar(20),
     color varchar(20),
     number int,
     driver_id int,
@@ -79,7 +45,7 @@ CREATE TABLE taxi1 (
 );
 
 CREATE TABLE taxi2 (
-    taxi_id int,
+    taxi_id varchar(20),
     model varchar(20),
     PRIMARY KEY(taxi_id),
     FOREIGN KEY(taxi_id) REFERENCES taxi1(taxi_id)
@@ -93,8 +59,42 @@ CREATE TABLE taxi3 (
 
 );
 
+CREATE TABLE driver1 (
+    driver_id varchar(20),
+    d_name varchar(20),
+    d_phone_no int, 
+    taxi_id varchar(20), 
+    rating int,
+    PRIMARY KEY (driver_id, taxi_id),
+    FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id)
+);
+
+CREATE TABLE driver2 (
+    driver_id varchar(20), 
+    d_phone_no int,
+    PRIMARY KEY (driver_id),
+    FOREIGN KEY (driver_id) REFERENCES driver1(driver_id)
+);
+
+CREATE TABLE driver3 (
+    d_phone_no int, 
+    d_name varchar(20),
+    PRIMARY KEY (d_phone_no)
+);
+
+CREATE TABLE garage (
+    garage_id varchar(20), 
+    taxi_id varchar(20), 
+    status varchar(20),
+    PRIMARY KEY (garage_id, taxi_id),
+    FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id)
+);
+ 
+
+
+
 CREATE TABLE trip1 (
-    trip_id int,
+    trip_id varchar(20),
     start time, 
     end time, 
     duration time,
@@ -103,7 +103,7 @@ CREATE TABLE trip1 (
 );
 
 CREATE TABLE trip2(
-    trip_id int, 
+    trip_id varchar(20), 
     from_s varchar(20),
     to_d varchar(20),
     duration time,
@@ -112,51 +112,51 @@ CREATE TABLE trip2(
 );
 
 CREATE TABLE trip3 (
-    user_id int,
+    user_id varchar(20),
     from_s varchar(20),
     to_d varchar(20),
-    trip_id int,
+    trip_id varchar(20),
     PRIMARY KEY (trip_id, user_id),
     FOREIGN KEY (user_id) REFERENCES user1(user_id)
 );
 
 CREATE TABLE trips4(
-    trip_id int,
+    trip_id varchar(20),
     rating int,
     status varchar(10),
-    taxi_id int,
-    driver_id int,
+    taxi_id varchar(20),
+    driver_id varchar(20),
     PRIMARY KEY (trip_id, taxi_id, driver_id),
     FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id),
     FOREIGN KEY (driver_id) REFERENCES driver1(driver_id)
 );
 
 CREATE TABLE books (
-    user_id int,
-    trip_id int,
+    user_id varchar(20),
+    trip_id varchar(20),
     PRIMARY KEY(user_id, trip_id),
     FOREIGN KEY (user_id) REFERENCES user1(user_id),
     FOREIGN KEY (trip_id) REFERENCES trip1(trip_id)
 );
 
 CREATE TABLE works (
-    driver_id int,
-    shift_id int,
+    driver_id varchar(20),
+    shift_id varchar(20),
     PRIMARY KEY (driver_id, shift_id),
     FOREIGN KEY (driver_id) REFERENCES driver1(driver_id),
     FOREIGN KEY (shift_id) REFERENCES shifts(shift_id)
 );
 
 CREATE TABLE drives (
-    driver_id int, 
-    taxi_id int,
+    driver_id varchar(20), 
+    taxi_id varchar(20),
     PRIMARY KEY (driver_id, taxi_id),
     FOREIGN KEY (driver_id) REFERENCES driver1(driver_id),
     FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id)
 );
 
 CREATE TABLE availability (
-    taxi_id int,
+    taxi_id varchar(20),
     zipcode int,
     PRIMARY KEY(taxi_id, zipcode),
     FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id),
@@ -165,7 +165,7 @@ CREATE TABLE availability (
 
 
 CREATE TABLE present_at (
-    driver_id int,
+    driver_id varchar(20),
     zipcode int,
     PRIMARY KEY (driver_id, zipcode),
     FOREIGN KEY (driver_id) REFERENCES driver1(driver_id),
@@ -173,17 +173,18 @@ CREATE TABLE present_at (
 );
 
 CREATE TABLE used_for(
-    taxi_id int,
-    trip_id int,
+    taxi_id varchar(20),
+    trip_id varchar(20),
     PRIMARY KEY (taxi_id, trip_id),
     FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id),
     FOREIGN KEY (trip_id) REFERENCES trip1(trip_id)
 );
 
 CREATE TABLE contains (
-    taxi_id int,
-    garage_id int,
+    taxi_id varchar(20),
+    garage_id varchar(20),
     PRIMARY KEY (taxi_id, garage_id),
     FOREIGN KEY (taxi_id) REFERENCES taxi1(taxi_id),
     FOREIGN KEY (garage_id) REFERENCES garage(garage_id)
 );
+
