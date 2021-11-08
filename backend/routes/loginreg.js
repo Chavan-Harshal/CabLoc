affa065a88a1f8e2f367baefff1cb80d66429d68
NikitaTipule <nikitatipule@gmail.com>
2021-11-08 13:08:59 +0530

    routes for customer registration and login done

---
+++ backend/routes/loginreg.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const connection = require("../connection");
+
+// route for user registration
+router.post("/register", async (req, res) => {
+  //   console.log(req);
+  console.log(req.body);
+  const { name, pass, add, ph } = req.body;
+  //   return res.status(200).json("success");
+  const que = `INSERT INTO user1 values("${pass}", "${name}"); INSERT INTO user2 values("${pass}", "${ph}"); INSERT INTO user3 values("${pass}", "${add}"); INSERT INTO user4 values("${ph}", "${name}")`;
+  connection.query(`${que}`, [1, 2, 3, 4], (e, op) => {
+    if (e) {
+      console.log(e);
+      return res.status(404).json({ msg: "Error" });
+    } else {
+      return res.status(200).json({ msg: "Successfully Registered" });
+    }
+  });
+});
+
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  const { name, pass } = req.body;
+  console.log(req.body);
+  connection.query(
+    `SELECT * FROM user1 WHERE name="${name}" and user_id="${pass}";`,
+    (e, op) => {
+      if (e) {
+        console.log(e);
+        return res.status(404).json({ msg: "Some error occured" });
+      } else {
+        if (op.length === 0) {
+          console.log("yes");
+          return res.status(200).json({ msg: "Invalid creds" });
+        } else {
+          return res.status(200).json({ msg: "Success", data: op });
+        }
+      }
+    }
+  );
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const connection = require("./connection");
-
+const router = require("./routes/loginreg");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,3 +12,5 @@ app.use(cors());
 app.listen(port, () => {
   console.log(`Listening to ${port}`);
 });
+
+app.use("/api", router);
